2cf444d9e39bae2a55798bcca7ab4ed9e75c9a6c
unknown <gldud9833@gmail.com>
2019-01-19 10:25:40 -0500

    added typescript support

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,41 +1,80 @@
-"use strict";
-
-let myGlobalVariable = 0;
-
-// Selft-Executing Anonymous Function
-// ITFE - Immediately Invoked Functon Expression
-(function(){
-    let myFunctionVariable = 0;
-
-    /*
-    // Version #1
-    function Start() {
-        let startVariable = 0;
-        console.log("App Started...");
-    }
-
-    // Version #2 
-    //window.addEventListener("load", function () {
-    window.addEventListener("load", ()=> {
-        let startVariable = 0;
-        console.log("App Started...");
-    });
-    
-
-    // Version #3
-    let Start = function() {
-        let startVariable = 0;
-        myFunctionVariable = "Heeyeong";
-
-        //console.log("%c App Started..." + startVariable, "font-size: 20px; color:blue;");
-        console.log(`%c App Started... ${myFunctionVariable}`, "font-size: 20px; color:blue;");
-    }
-   // window.addEventListener("load", Start);
-    */
-   
-    function Start() {
-        // log, info, warn, error
-        console.log(`%c App Started...`, "font-size: 20px; color:blue;");
-    }
-    window.addEventListener("load", Start);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
 })();
+var objects;
+(function (objects) {
+    var Person = /** @class */ (function () {
+        function Person(age, name) {
+            this._age = age; // possible to use this.age, this.name (using getter)
+            this._name = name;
+        }
+        Object.defineProperty(Person.prototype, "age", {
+            // PUBLIC PROPERTIES
+            get: function () {
+                return this._age;
+            },
+            set: function (newAge) {
+                this._age = newAge;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Person.prototype, "name", {
+            get: function () {
+                return this._name;
+            },
+            set: function (newName) {
+                this._name = newName;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Person.prototype.saysHello = function () {
+            console.log(this.name + " says Hello!");
+        };
+        return Person;
+    }());
+    objects.Person = Person;
+    var Student = /** @class */ (function (_super) {
+        __extends(Student, _super);
+        function Student(age, name, studentID) {
+            var _this = _super.call(this, age, name) || this;
+            _this.studentID = studentID;
+            return _this;
+        }
+        Object.defineProperty(Student.prototype, "studentID", {
+            // PUBLIC PROPERTIES
+            get: function () {
+                return this._studentID;
+            },
+            set: function (newStudentID) {
+                this._studentID = newStudentID;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        // PUBLIC METHODS
+        Student.prototype.studies = function () {
+            console.log(this.name + " with studentID: " + this.studentID + " is studying.");
+        };
+        return Student;
+    }(objects.Person));
+    objects.Student = Student;
+})(objects || (objects = {}));
+//let person = new objects.Person(25, "Ellie"); // ver01
+var person = new objects.Person(25, "Ellie");
+person.saysHello();
+var student = new objects.Student(20, "Sarah", "P12345");
+student.saysHello();
+student.studies();
+//# sourceMappingURL=app.js.map
